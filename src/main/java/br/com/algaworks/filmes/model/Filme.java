7cc68c3bb658e1adc18651bb244b1f3733ea01f4
Marcos Souza <marcossouza.git@gmail.com>
2022-09-16 13:28:56 -0300

    Criado a Pasta model, Criacao da classe filme com os construtores @AllArgsConstructor

---
+++ src/main/java/br/com/algaworks/filmes/model/Filme.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.filmes.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Filme {
+
+    private Long codigo;
+    private String titulo;
+    private String descricao;
+}
